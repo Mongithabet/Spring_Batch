7c58dc267d7041ce639e205835560ce92c23f17c
Mongithabet <stechnopc@yahoo.fr>
2021-10-04 19:18:45 +0100

    sec

--- src/main/java/org/sid/bankspringbatch/BankTransactionItemProcessor.java
+++ src/main/java/org/sid/bankspringbatch/BankTransactionItemProcessor.java
@@ -1,4 +1,20 @@
 package org.sid.bankspringbatch;
 
-public class BankTransactionItemProcessor {
-}
+import org.sid.bankspringbatch.metier.BankTransaction;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+
+@Component
+public class BankTransactionItemProcessor implements ItemProcessor<BankTransaction,BankTransaction> {
+private SimpleDateFormat dateFormat=new SimpleDateFormat("dd/MM/yyyy-HH:mm");
+@Override
+            public BankTransaction process(BankTransaction bankTransaction) throws Exception {
+               bankTransaction.setTransactionDate(dateFormat.parse(bankTransaction.getStrTransactionDate()));
+                return bankTransaction;
+            }
+        }
+
+
--- src/main/java/org/sid/bankspringbatch/BankTransactionItemWriter.java
+++ src/main/java/org/sid/bankspringbatch/BankTransactionItemWriter.java
@@ -1,2 +1,18 @@
-package org.sid.bankspringbatch;public class BankTransactionItemWriter {
+package org.sid.bankspringbatch;
+
+import org.sid.bankspringbatch.dao.BankTransactionRepository;
+import org.sid.bankspringbatch.metier.BankTransaction;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+@Component
+public class BankTransactionItemWriter implements ItemWriter<BankTransaction> {
+  @Autowired
+   private BankTransactionRepository bankTransactionRepository;
+    @Override
+    public void write(List<? extends BankTransaction> list) throws Exception {
+bankTransactionRepository.saveAll(list);
+    }
 }
